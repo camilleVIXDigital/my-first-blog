cf4796ffdd6c072976c35cd9f2b9f3d2939ed419
camilleVIXDigital <camille@vix.digital>
2018-01-30 14:44:37 +0000

    better nav & Foundation/SASS/JS

--- blog/static/js/custom/init-foundation.js
+++ blog/static/js/custom/init-foundation.js
@@ -1,1 +1,36 @@
 $(document).foundation();
+
+
+// --- ANIMATION MENU TWIST
+
+$(document).ready(function(){
+	$('#nav-icon').click(function(){
+		$(this).toggleClass('open');
+	});
+});
+
+
+
+// --- ANIMATION SHAKE
+
+function hoverShake(myElement, speed, deg) {
+	$(myElement).mouseenter(function() {
+		myInterval = setInterval(function() {
+			$(myElement).css('transform', 'rotate('+deg+'deg)');
+			myTimeout = setTimeout(function() {
+				$(myElement).css('transform', 'rotate('+(360-deg)+'deg)');
+			}, speed);
+		}, speed * 2);
+	});
+	
+	$(myElement).mouseleave(function() {
+		clearInterval(myInterval);
+		clearTimeout(myTimeout);
+		$(myElement).css('transform', 'rotate(0deg)');
+	});
+}
+
+
+hoverShake('#link-mediasocial', 100, 5);
+hoverShake('#link-home', 100, 5);
+hoverShake('#link-test', 100, 5);
