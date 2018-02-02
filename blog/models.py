4de15ef1a394fe0f58572209459ed43cc6aae384
camilleVIXDigital <camille@vix.digital>
2018-02-02 15:25:56 +0000

    With new date

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
 	author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
 	title = models.CharField(max_length=200)
 	text = models.TextField()
-	post_by = models.CharField(max_length=200)
+	authors = models.CharField(max_length=200, default='Post By')
 	created_date = models.DateTimeField(
 		default=timezone.now)
 	published_date = models.DateTimeField(
